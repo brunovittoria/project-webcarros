c8e0417bc0ccf7ea66d994315ce605cd711204f6
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-02-27 21:34:27 +0100

    Database bug solved

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
     try {
     await mongoose.connect(
-        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@webcars.tgam586.mongodb.net/?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@webcars.tgam586.mongodb.net/?retryWrites=true&w=majority&appName=webcarsy`
     );
     console.log("Conexão ao DB realizada");
     } catch (error) {
